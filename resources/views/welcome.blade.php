2174818d3d6ceb8f19c27763d0140adc834f24bc
dhruv <dhruv.prajapati1@dcmail.ca>
2024-06-09 10:08:10 -0400

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dhruv Prajapati - Portfolio</title>
+</head>
+<body>
+    <h1>Dhruv</h1>
+    @vite('resources/js/app.js')
+</body>
+</html>
